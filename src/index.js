ad25c0db57fb118a7351683be9dedfdb9b0081d7
Calvin Chege <calvinchege1@gmail.com>
2024-04-09 20:48:06 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const createTaskForm = document.getElementById("create-task-form");
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTaskPriority = document.getElementById("new-task-priority")
+  const newTasksList = document.getElementById("tasks");
+  createTaskForm.addEventListener("submit", createNewTask);
+
 });
+const createNewTask = function (event) {
+  event.preventDefault();
+  const newTaskDescription = document.getElementById("new-task-description");
+  const newTask = document.createElement("li");
+  newTask.innerText = newTaskDescription.value;
+
+  appendNewTask(newTask);
+  event.target.reset(); 
+
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "X"
+newTask.appendChild(deleteBtn);
+deleteBtn.addEventListener("click", deleteTask)
+};
+
+const appendNewTask = function (task) {
+  document.getElementById("tasks").appendChild(task);
+}
+
+function deleteTask (event) {
+  event.target.parentNode.remove();
+}
